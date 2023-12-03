71e1fd2dfd3add588ad4efc5355034fd3ad44c5c
Sagarckr <sagarsharma9821315721@gmail.com>
2023-12-03 14:02:54 +0545

    rename

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let menu=document.querySelector('#menu-icon');
 let navbar=document.querySelector('.navbar');
-menu.onclick=()=>{
+menu.onclick = () =>{
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open')
 };
